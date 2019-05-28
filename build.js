577d32c6fa643508ab9bdc4ca97e2f4055a25f57
okayjava <coreaseo@gmail.com>
2019-05-28 15:41:59 +0900

    tag 1.3

--- build.js
+++ build.js
@@ -5,5 +5,6 @@ s.mkdir('-p', 'build/server/blockchain/common');
 s.mkdir('-p', 'build/server/blockchain/transaction');
 s.cp('.env', 'build/.env');
 s.cp('-R', 'public', 'build/public');
+s.cp('-R', 'server/common', 'build/server/');
 s.cp('-R', 'server/blockchain/common/*', 'build/server/blockchain/common');
 s.cp('-R', 'server/blockchain/transaction/*', 'build/server/blockchain/transaction');
